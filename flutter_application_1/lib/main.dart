d55c56e3e61d7b5ac0e4d1146cbaf0d37e36cc1b
meissnerM224 <109956564+meissnerM224@users.noreply.github.com>
2022-09-06 15:27:36 +0200

    BMI Calculator is ready

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -7,109 +7,140 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyBMIRechner(title: 'Bodymass Index Rechner'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MyBMIRechner extends StatefulWidget {
+  const MyBMIRechner({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyBMIRechner> createState() => _MyBMIRechnerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _MyBMIRechnerState extends State<MyBMIRechner> {
+  double _groesse = 1.80;
+  double _gewicht = 80.0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final bmi = _gewicht / (_groesse * _groesse);
+    var bmiSlider = bmi;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: LayoutBuilder(builder: ((context, constraints) {
+          double maxW = constraints.maxWidth * 0.75;
+          return Container(
+              color: Color.fromARGB(255, 30, 197, 212),
+              child: Center(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxWidth: maxW,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Größe in Meter:',
+                              style: TextStyle(
+                                  fontSize: 21.00,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color.fromARGB(255, 0, 0, 0)),
+                            ),
+                            Expanded(
+                              child: Slider(
+                                activeColor: Colors.green,
+                                inactiveColor:
+                                    Color.fromARGB(255, 188, 145, 95),
+                                divisions: 220,
+                                label: '${_groesse.toStringAsFixed(2)} m',
+                                onChanged: (value) {
+                                  setState(() {
+                                    _groesse = value;
+                                  });
+                                },
+                                min: 01.10,
+                                max: 02.20,
+                                value: _groesse,
+                              ),
+                            ),
+                          ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Gewicht in Kg:',
+                            style: TextStyle(
+                                fontSize: 21.00,
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromARGB(255, 0, 0, 0)),
+                          ),
+                          Expanded(
+                            child: Slider(
+                              activeColor: Colors.red,
+                              inactiveColor: Color.fromARGB(255, 188, 145, 95),
+                              divisions: 180,
+                              label: ' ${_gewicht.round()} Kg',
+                              onChanged: (value) {
+                                setState(() {
+                                  _gewicht = value;
+                                });
+                              },
+                              min: 20.0,
+                              max: 180.0,
+                              value: _gewicht,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Icon((bmi > 28 || bmi < 19)
+                          ? Icons.build_circle_sharp
+                          : Icons.check),
+                      Text(
+                        'Der BMI ist: ${bmi.toStringAsFixed(0)}',
+                        style: TextStyle(
+                          fontWeight: bmiSlider > 28 || bmiSlider < 19
+                              ? FontWeight.w700
+                              : FontWeight.w600,
+                          fontSize:
+                              bmiSlider > 28 || bmiSlider < 19 ? 23.00 : 21.00,
+                          color:
+                              bmi > 28 || bmi < 19 ? Colors.red : Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 600.00,
+                        height: 60.00,
+                        child: Slider(
+                          activeColor: bmiSlider > 28 || bmiSlider < 19
+                              ? Colors.red
+                              : Colors.black,
+                          value: bmiSlider,
+                          onChanged: (value) {},
+                          min: 4.00,
+                          max: 150.00,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ));
+        })));
   }
 }
